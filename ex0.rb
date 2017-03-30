fac65db8eff74e00ccbef784855d6f69fbaf4cf5
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-03-29 23:49:04 -0400

    ex0

---
+++ ex0.rb
@@ -0,0 +1,19 @@
+fav_colors = ['orange', 'red', 'kale', 'blue']
+age = [31, 41, 35, 32, 36, 30]
+coin = ['no', 'no', 'yes', 'no', 'yes']
+fav_artist = ['britney spears', 'grimes', 'lady gaga']
+fav_colors = [:orange, :red, :kale, :blue]
+
+
+definitions = {dance: "to move one's feet or body, or both, rhythmically in a pattern of steps, especially to the accompaniment of music.", sing: "to perform a song or voice composition", play: "exercise or activity for amusement or recreation."}
+movies =   [
+  { :name => 'Mean Girls', :year => 2004 },
+  { :name => 'The Holiday', :year => 2006 },
+  { :name => 'First Wives Club', :year => 1996 },
+]
+cities = {toronto: 2600000, montreal: 1650000, berlin: 3500000}
+people = {kevin: 35, cindy: 41, joyce: 32, harpreet: 30}
+
+
+print coin
+print fav_colors[0]
